e2154013f3cca408c2a9dd60721a5499166fc004
Rados <rados.pavlovic@euroinvest.si>
2025-12-15 19:14:06 +0100

    prvi commit course

---
+++ main/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>  
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_sleep.h"
+#include "esp_log.h"
+#include "esp32/rom/uart.h"
+
+
+void app_main(void)
+{
+
+}
